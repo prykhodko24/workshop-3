81abd924b6315486aa35a8befe7af9866eea44e3
tanya-24-01-2001 <44785259+tanya-24-01-2001@users.noreply.github.com>
2020-05-04 18:53:57 +0300

    Add files via upload

---
+++ loop.sql
@@ -0,0 +1,19 @@
+
+Declare
+   type arr_type_small IS VARRAY(6) OF VARCHAR2(10);
+   type arr_type_big IS VARRAY(6) OF VARCHAR2(30);   
+
+city_name arr_type_big;
+country_country_name arr_type_small;
+BEGIN
+city_name:=arr_type_big('London','Lviv','Kyev' ,'Odesa','NY','LA');
+country_country_name:=arr_type_small('UK','UA','UA','UA','USA','USA');
+
+    FOR i IN 1 .. 6 
+    LOOP
+        INSERT INTO city (city_name, country_country_name) VALUES (city_name(i), country_country_name(i));
+        COMMIT;
+    END LOOP;
+END;
+
+
